342cdf5d527ed468aac1a5c4a9976d89a075a2fc
username <email>
2020-09-14 20:56:05 +0800

    增加注册登录模块

--- ApiTest/MyApp/views.py
+++ ApiTest/MyApp/views.py
@@ -1,10 +1,58 @@
-from django.shortcuts import render
-
 # Create your views here.
 #HttpResponse用来返回一个字符串的，HttpResponseRedirect是用来重定向到其他url上，render是用来返回html页面和页面初始数据
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required    #登录态检查
 
+@login_required
 def welcome(request):
-    print('进来啦……')
     return render(request,'welcome.html')
+
+#返回子页面
+def child(request,eid,oid):
+    return render(request,eid)
+
+#进入主页
+@login_required
+def home(request):
+    return render(request,'welcome.html',{"whichHTML": "Home.html","oid": ""})
+
+#进入登录页面
+def login(request):
+    return render(request,'login.html')
+
+#登陆
+def login_action(request):
+    u_name = request.GET['username']
+    p_word = request.GET['password']
+    #连Django用户库，查看用户名/密码是否正确
+    from django.contrib import auth
+    user = auth.authenticate(username=u_name,password=p_word)
+    if user is not None:
+        auth.login(request,user)   #检查用户是否已登录
+        request.session['user'] = u_name  #记录用户登录态
+        return HttpResponse('成功')
+    else:
+        #返回前端告诉前端用户名/密码不对
+        return HttpResponse('失败')
+#注册
+def register_action(request):
+    u_name = request.GET['username']
+    p_word = request.GET['password']
+    #连通Django用户表
+    from django.contrib.auth.models import User
+    try:
+        user = User.objects.create_user(username=u_name,password=p_word)
+        user.save()
+        return HttpResponse('注册成功')
+    except:
+        return HttpResponse('注册失败,用户已存在……')
+
+def case_list(request):
+    return HttpResponse('测试用例列表……')
+
+def item_list(request):
+    return HttpResponse('项目列表……')
+
+def portlib(request):
+    return HttpResponse('接口库……')
